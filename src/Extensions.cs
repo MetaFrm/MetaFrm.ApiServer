f1ba64e4d439294dd01ba28630bc98f95ebe9e7d
DSUN <dsun@hanmail.net>
2024-07-19 13:27:03 +0900

    2024-07-19_20_01

---
+++ src/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using MetaFrm.Extensions;
+using MetaFrm.Maui.Devices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MetaFrm.ApiServer
+{
+    /// <summary>
+    /// Extensions
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// AddMetaFrm
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMetaFrm(this IServiceCollection services)
+        {
+            services.AddFactory(new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true), DevicePlatform.Server);
+
+            return services;
+        }
+    }
+}
